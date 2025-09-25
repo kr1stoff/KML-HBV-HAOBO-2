3c5449c794f462517b814ffd41d2ecd4929a5a47
kristoff <706060951@qq.com>
2025-09-25 13:35:53 +0800

    Add initial environment and ignore files
    Added .gitignore for Python projects and three Conda environment YAML files (basic.yaml, basic2.yaml, python3.12.yaml) under src/workflow/env to define reproducible environments for different workflows.

--- src/kml_hbv_haobo_2/cli.py
+++ src/kml_hbv_haobo_2/cli.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from src.kml_hbv_haobo_2.fastq import prepare_fastq_by_samptab
 from src.kml_hbv_haobo_2.snakemake import run_snakemake
+from src.kml_hbv_haobo_2 import __version__
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -12,6 +13,7 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 @click.option('--input-tab', required=True, help='输入样本表, 包含样本名, read1 和 read2 的路径')
 @click.option('--output-dir', default='kml-hbv-haobo-result', show_default=True, help='输出文件夹')
 @click.option('--threads', default=8, type=int, show_default=True, help='线程数')
+@click.version_option(version=__version__, message='KML HBV HAOBO Version: %(version)s', help='显示版本信息')
 @click.help_option(help='获取帮助信息')
 def main(input_tab, output_dir, threads):
     """KML 浩博 HBV X 基因变异分析流程"""
