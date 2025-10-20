7ec04092794626c95696b1681c5357da13ccaf5f
kristoff <706060951@qq.com>
2025-10-20 10:06:24 +0800

    20251020

--- src/config/database.py
+++ src/config/database.py
@@ -1,7 +1,7 @@
 DATABASE = {
     # * 参考基因组先用 bwa,samtools 建立索引
     'ref': '/data/mengxf/GitHub/KML-HBV-HAOBO-2/assets/D00330/D00330.fa',
-    'bed': '/data/mengxf/GitHub/KML-HBV-HAOBO-2/assets/D00330/target-500.bed',
+    'bed': '/data/mengxf/GitHub/KML-HBV-HAOBO-2/assets/D00330/target-1800.bed',
     'known_sites': '/data/mengxf/GitHub/KML-HBV-HAOBO-2/assets/known_sites.csv',
     'low_cpx': '/data/mengxf/GitHub/KML-HBV-HAOBO-2/assets/D00330/low_cpx.bed',
 }
