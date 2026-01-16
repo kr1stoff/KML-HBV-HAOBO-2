8e69512354843d38f39cdca17686e40668a2c746
kristoff <706060951@qq.com>
2026-01-16 11:01:03 +0800

    Update all_qc_summary.py

--- src/workflow/scripts/all_qc_summary.py
+++ src/workflow/scripts/all_qc_summary.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import sys
+import numpy as np
 
 sys.stderr = open(snakemake.log[0], "w")
 
 dfqc = pd.read_csv(snakemake.input[0], sep="\t")
 dfbam = pd.read_csv(snakemake.input[1], sep="\t")
 dfmerged = pd.merge(dfqc, dfbam, on='Sample', how='left')
+
+# * [20260112 HWY SOP QC 参数] 1.≥0.35Gb, 2. Q30≥85%
+# 使用numpy的where函数生成QC结果，大型数据集下性能最优
+dfmerged.insert(
+    1,
+    'QCJudgment',
+    np.where((dfmerged['RawBases'] >= 0.35*1e9) &
+             (dfmerged['CleanQ30'] >= 0.85), 'Pass', 'Fail')
+)
 
 # 输出结果
 dfmerged.to_csv(snakemake.output[0], index=False, sep='\t')
